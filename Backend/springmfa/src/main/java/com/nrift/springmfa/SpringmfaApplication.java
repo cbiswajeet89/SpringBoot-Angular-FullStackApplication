bbfd45cb66854442b3e869aa8c0a70d13d75bac3
Biswajeet Chakraborty <cbiswajeet89@gmail.com>
2023-12-24 20:41:51 +0530

    created springboot backend project

---
+++ Backend/springmfa/src/main/java/com/nrift/springmfa/SpringmfaApplication.java
@@ -0,0 +1,13 @@
+package com.nrift.springmfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmfaApplication.class, args);
+	}
+
+}
---
+++ Backend/springmfa/src/test/java/com/nrift/springmfa/SpringmfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nrift.springmfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
